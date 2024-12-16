2e3926469a2e9ceccba89f3d086cce6d6bd30463
Fiona He <fiona.he@ontariohealth.ca>
2024-12-16 12:43:15 -0500

    This is Fiona He(Fiona0619), who is submitting assignment-shell.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -9,7 +9,7 @@ set -x
 # project name and a brief description of the project.
 # Then it unzips the raw data provided by the client.
 
-mkdir analysis output
+mkdir analysis_output
 touch README.md
 touch analysis/main.py
 
@@ -26,46 +26,41 @@ mkdir -p ./data
 # Commend 'mv ./rawdata ./data/raw' will do this. If the data directory doesn't exist, create it then run the commend.
 mkdir -p ./data
 mv ./rawdata ./data/raw
-#Verify the move:
-Is ./data
+
 # If the data show up, the moving is successiful.
 
 # 3. List the contents of the ./data/raw directory
 #To list the contents of the ./data/raw directory, you can use the ls command:
-ls ./data/raw
+List ./data/raw
 
-
-# 3. List the contents of the ./data/raw directory
-ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 # Command:
 mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 # Verify the Directory Creation, list the contents of './data/processed':
-Is ./data/processed
+list ./data/processed
 # server_logs, user_logs, and event_logs directions would be seen.
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 # Run the command:
-cp ./data/raw/*server*.log  ./data/processed/server_logs/
+cp ./data/raw/*server*.log  ./data/processed/server_logs
 # To confirm that the files were copied, list the contents of destination:
-Is ./data/processed/server_logs
+list ./data/processed/server_logs
 #You should see all the relevant files with "server" in their names and a .log extension.
 
 # 6. Repeat the above step for user logs and event logs
 #Copy files
-cp ./data/raw/*user*.log  ./data/processed/user_logs/
-cp .data/raw/*event*.log  ./data/processed/event_logs/
+cp ./data/raw/*user*.log  ./data/processed/user_logs
+cp ./data/raw/*event*.log  ./data/processed/event_logs
 # Confirm that the files were copied
-Is ./data/processed/user_logs
-Is ./data/processed/event_logs
+list ./data/processed/user_logs
+list ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
- touch ./data/inventory.txt ./data/processed
-touch ./data/inventory.txt ./data/processed
+ touch ./data/inventory.txt 
 
 
 
